3c620ae641bac7d1cd1ab9acaa723010d5534549
shivani brijmohan <sbrij001@yahoo.com>
2018-12-02 19:03:09 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -26,13 +26,11 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
+  if !valid_move?(board, input)
     turn(board)
   end
+  move(board,input,current_player(board))
+  display_board(board)
 end
 
 # Define your play method below
@@ -40,14 +38,3 @@ end
 #start a loop and call turn
 #max of 9 turns to be played
 #use a while/until loop and make sure to exit loop
-
-def play(board)
-  display_board(board)
-
-  turns = 0
-  until turns == 9
-    turn(board)
-    turns += 1
-  end
-  
-end
